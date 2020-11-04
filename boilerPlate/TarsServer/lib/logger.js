0ce4e2bde59a69e56d018a14e654516cd75e93f7
Bellhe <bellhe@upchina.com>
2020-11-04 21:28:40 +0800

    添加 TarsServer 模板

---
+++ boilerPlate/TarsServer/lib/logger.js
@@ -0,0 +1,8 @@
+const tarsLogs = require('@tars/logs');
+
+let logger = {};
+logger.error = new tarsLogs('TarsDate', 'error');
+logger.accessLog = new tarsLogs('TarsDate', 'accessLog');
+logger.server = new tarsLogs('TarsDate', 'server');
+
+module.exports = logger;
